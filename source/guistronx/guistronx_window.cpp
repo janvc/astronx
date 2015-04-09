9a6220a06928d26af40d474adffef0a967441ba0
Jan von Cosel <jvc_astronx@gmx.de>
2015-04-09 13:38:22 +0200

    added copyright headers to all new files

--- source/guistronx/guistronx_window.cpp
+++ source/guistronx/guistronx_window.cpp
@@ -1,3 +1,24 @@
+/*
+ * Copyright 2015 Jan von Cosel
+ *
+ * This file is part of astronx.
+ *
+ * astronx is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * astronx is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have recieved a copy of the GNU General Public License
+ * along with molconv. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+
 #include "guistronx_window.h"
 #include "ui_guistronx_window.h"
 
