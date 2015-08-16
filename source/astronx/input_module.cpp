267ce96fb0bb0e147c3827d07a0bfd8044181ad5
Jan von Cosel <jvc_astronx@gmx.de>
2015-08-16 13:29:32 +0200

    started a rewrite of astronx in C++

---
+++ source/astronx/input_module.cpp
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2015 Jan von Cosel
+ *
+ * This file is part of astronx.
+ *
+ * astronx is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * astronx is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have recieved a copy of the GNU General Public License
+ * along with astronx. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+
+#include "input_module.h"
+
+namespace astronx
+{
+
+void input::process_cmd_arguments()
+{
+}
+
+void input::read_input_file(const std::string &input_file, Eigen::MatrixXd &X, Eigen::MatrixXd &V)
+{
+}
+
+} // namespace astronx
